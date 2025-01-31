2d7c5bc59c3b387a58e00054e6569d8029ee0837
Om Rajesh More <moreom708@gmail.com>
2025-01-31 11:17:28 +0530

    Update firebase.jsx

---
+++ blogging website - frontend/src/common/firebase.jsx
@@ -0,0 +1,37 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyALo46rqUMZD0GaX6qXbISocwgRUtgVrMg",
+    authDomain: "mern-blogging-website-7ec72.firebaseapp.com",
+    projectId: "mern-blogging-website-7ec72",
+    storageBucket: "mern-blogging-website-7ec72.firebasestorage.app",
+    messagingSenderId: "28227299497",
+    appId: "1:28227299497:web:164b397285236c2d8ea1b8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+//google auth
+
+const provider = new GoogleAuthProvider()
+
+const auth = getAuth();
+
+export const authWithGoogle = async () => {
+
+    let user = null;
+
+    await signInWithPopup(auth, provider)
+        .then((result) => {
+            user = result.user
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+
+    return user;
+}
